5e9a2b85a964e5199c8bf093af0a49f8b17d8218
SHARON ZHENG <sharzheng@gmail.com>
2019-02-02 17:37:15 -0500

    Updates

---
+++ precache-manifest.e95805ab7099e17fe2baf7d4cf0dfc87.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ef4bb8cf95808b4614fd3c6cc49dea9",
+    "url": "/tulum/static/media/tacos.8ef4bb8c.jpeg"
+  },
+  {
+    "revision": "81101214f43908f58bb9846516410430",
+    "url": "/tulum/static/media/raspericos.81101214.jpeg"
+  },
+  {
+    "revision": "aae4e9149485a4ec8be21a87dcbb0ef8",
+    "url": "/tulum/static/media/jamaica.aae4e914.jpeg"
+  },
+  {
+    "revision": "65ef1765dbfb2bd20d9bf54d2b5e0517",
+    "url": "/tulum/static/media/hot-sauce.65ef1765.jpeg"
+  },
+  {
+    "revision": "1a638da3f23986a24d17",
+    "url": "/tulum/static/js/runtime~main.1a638da3.js"
+  },
+  {
+    "revision": "2069bc019b8c1b4c694d",
+    "url": "/tulum/static/js/main.2069bc01.chunk.js"
+  },
+  {
+    "revision": "8e758ad169d171081bae",
+    "url": "/tulum/static/js/1.8e758ad1.chunk.js"
+  },
+  {
+    "revision": "2069bc019b8c1b4c694d",
+    "url": "/tulum/static/css/main.54c8dc17.chunk.css"
+  },
+  {
+    "revision": "0944a6793950dea06ed3b152fd428fa5",
+    "url": "/tulum/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/tulum/precache-manifest.e95805ab7099e17fe2baf7d4cf0dfc87.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/tulum/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
